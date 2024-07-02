dbcd8bbf4112401daa9a310bffbef035605b33b5
unknown <ian.marcelo.perez@gmail.com>
2024-07-02 17:51:28 -0500

    me mandaron a mi casa

---
+++ Assets/Scripts/CamaraBarradevida.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraBarradevida : MonoBehaviour
+{
+    public Transform camara;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
